18eab6993f5e5ceeb8e8567892cdf00126e1a423
clovis <clovisgladstone@gmail.com>
2026-04-10 11:40:52 -0500

    add lookup word script placeholder

---
+++ www/scripts/lookup_word.py
@@ -0,0 +1,6 @@
+"""Empty function to be used as a placeholder for the lookup word functionality."""
+
+
+def lookup_word(request, config):
+    """Placeholder function for the lookup word functionality."""
+    return {"message": "This is a placeholder for the lookup word functionality."}
